91f5f4a9458d9c0a1562c67268dbffc15b4b6821
Francisco-0510 <francisco109623@gmail.com>
2024-09-14 11:34:08 -0600

    Info Header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'infinite-scroll': 'infinite-scroll 25s linear infinite',
+				'ping': 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite'
+			},
+			keyframes: {
+				'infinite-scroll': {
+				from: { transform: 'translateX(0)' },
+				to: { transform: 'translateX(-100%)' }
+				},
+				'ping': {
+					'0%': { transform: 'scale(1)', opacity: '1' },
+					'75%, 100%': { transform: 'scale(2)', opacity: '0'}
+				}
+			},
+		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
